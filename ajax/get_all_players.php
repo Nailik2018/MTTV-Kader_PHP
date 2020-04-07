5b5a0c35935b70ddd7739fe54cc920d54fc4f836
kilianalthaus <kilualthaus@hotmail.com>
2020-04-07 23:11:54 +0200

    datenbank verbindung per inifile, Alle Spieler ausgabe json String

--- ajax/get_all_players.php
+++ ajax/get_all_players.php
@@ -1,3 +1,9 @@
 <?php
+require_once "../collections/DataBase.php";
 
+header('Content-Type: application/json');
+
+$db = new DataBase();
+$db->connection();
+print_r($db->getAllPlayers("SELECT * FROM players"));
 
